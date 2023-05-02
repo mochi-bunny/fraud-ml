0dbb48c3e8d1b7da9afd30c67b724e3097a21478
mochi-bunny <99592507+mochi-bunny@users.noreply.github.com>
2023-05-02 16:41:36 +0400

    Update app.py.py

--- app.py.py
+++ app.py.py
@@ -8,8 +8,8 @@ import streamlit as st
 
 st.title('Interactive Text Input Example')
 st.write('enter transactions details')
-v0 = st.number_input(label=“v0”,step=1.,format="%.2f")
-v1 = st.number_input(label=“v1”,step=1.,format="%.2f")
+v0 = st.number_input(label='v0',step=1.,format="%.2f")
+v1 = st.number_input(label='v1',step=1.,format="%.2f")
 
 
 # In[ ]:
