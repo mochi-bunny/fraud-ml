1452990bba4552b34402314ff83efecde51e8e7e
mochi-bunny <99592507+mochi-bunny@users.noreply.github.com>
2023-05-02 16:38:12 +0400

    Add files via upload

---
+++ app.py.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+import streamlit as st
+
+st.title('Interactive Text Input Example')
+st.write('enter transactions details')
+v0 = st.number_input(label=“v0”,step=1.,format="%.2f")
+v1 = st.number_input(label=“v1”,step=1.,format="%.2f")
+
+
+# In[ ]:
+
+
+
+
